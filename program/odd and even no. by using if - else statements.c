1b9e7c8f1b4a4d0883814a70b4b7116ffc423308
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2020-09-19 01:06:55 +0530

    Add files via upload

---
+++ program/odd and even no. by using if - else statements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	printf(" \n enter the value of a",a);
+	scanf("%d \n",&a);
+	if(a%2==0){
+		printf("\n it is an even no.  \n",a);
+	}
+		else{
+		
+		printf("\n it is an odd no.  \n");
+	}
+	// your code goes here
+	return 0;
+}
